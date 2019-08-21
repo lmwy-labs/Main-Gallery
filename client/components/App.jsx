0aadb79ec4517d01eacbe7272a5414ebac4daaec
Howard Yang <howardyang25@gmail.com>
2019-08-20 19:13:29 -0700

    Complete CSS for lower picture gallery. Pictures are hardcoded URLs

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      Hello world!
+    <div class="picture-row">
+      <div class="picture-column">
+        <img src="https://resizer.otstatic.com/v2/photos/large/25162646.jpg" height="138.77" width="138.77"></img>
+        <img src="https://resizer.otstatic.com/v2/photos/large/23369781.jpg" height="138.77" width="138.77"></img>
+      </div>
+      <div class="picture-column">
+        <img src="https://resizer.otstatic.com/v2/photos/large/23369775.jpg" height="279.53" width="279.53"></img>
+      </div>
+      <div class="picture-column">
+        <img src="https://resizer.otstatic.com/v2/photos/large/25162645.jpg" height="91.84" width="91.84"></img>
+        <img src="https://resizer.otstatic.com/v2/photos/large/25841202.jpg" height="91.84" width="91.84"></img>
+        <img src="https://resizer.otstatic.com/v2/photos/large/25841200.jpg" height="91.84" width="91.84"></img>
+      </div>
+      <div class="picture-column">
+        <img src="https://resizer.otstatic.com/v2/photos/large/23369782.jpg" height="91.84" width="91.84"></img>
+        <img src="https://resizer.otstatic.com/v2/photos/large/25841201.jpg" height="91.84" width="91.84"></img>
+        <img src="https://resizer.otstatic.com/v2/photos/large/25841199.jpg" height="91.84" width="91.84"></img>
+      </div>
     </div>
   );
 };
