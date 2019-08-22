f6326d516fc51e75079891c0400b2aa41c59cd63
Howard Yang <howardyang25@gmail.com>
2019-08-22 12:18:47 -0700

    Failed to configure test using Axios request. Will try again later

--- client/components/Gallery/Gallery.test.js
+++ client/components/Gallery/Gallery.test.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import Gallery from './Gallery.jsx';
 
 
 describe('Gallery', () => {
   test('testing', () => {
-    fetch('/benu', {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    .then((response) => response.json())
-    .then((images) => {
-       const wrapper = shallow(
-        <Gallery images={images}/>
-       );
-       expect(wrapper).toMatchSnapshot();
-    });
+    const wrapper = shallow(
+      <Gallery images={[]}/>
+      );
+      expect(wrapper).toMatchSnapshot();
   });
 });
